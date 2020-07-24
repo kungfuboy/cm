8982d025a7822235b1d0a031decfd71a0b17cdb0
songjunjie <songjunjie@ygsoft.com>
2020-07-24 08:23:38 +0800

    将参数广播出来

--- vim-save-and-run.js
+++ vim-save-and-run.js
@@ -2,25 +2,19 @@
   if (typeof exports == "object" && typeof module == "object")
     // CommonJS
     mod();
-  // mod(
-  //   require("../lib/codemirror"),
-  //   require("../addon/search/searchcursor"),
-  //   require("../addon/dialog/dialog"),
-  //   require("../addon/edit/matchbrackets.js")
-  // );
   else if (typeof define == "function" && define.amd)
     // AMD
-    define([
-      //   "../lib/codemirror",
-      //   "../addon/search/searchcursor",
-      //   "../addon/dialog/dialog",
-      //   "../addon/edit/matchbrackets",
-    ], mod);
+    define([], mod);
   // Plain browser env
   else mod(CodeMirror);
 })(function (CodeMirror) {
   "use strict";
   CodeMirror.Vim.defineEx("!", "", function (cm, params) {
-    CodeMirror.signal(cm, "save-run", "Save & run");
+    console.log(params);
+    CodeMirror.signal(
+      cm,
+      "save-run",
+      "Save & run" + "\n参数是：" + params.args || ""
+    );
   });
 });
